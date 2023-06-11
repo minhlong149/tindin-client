bdaa84d90e6a5b892ede212d00e9c1c74936b7ec
Xuan Bien <>
2023-06-11 15:50:44 +0700

    commit

--- src/components/Organization/Organization.jsx
+++ src/components/Organization/Organization.jsx
@@ -1,4 +1,4 @@
-import { Typography } from '@mui/material';
+import { ListItem, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import Stack from '@mui/material/Stack';
 import { useParams } from 'react-router-dom';
@@ -49,48 +49,66 @@ export default function Organization() {
   }, [job]);
 
   return (
+    <List
+      sx={{
+        width: '100%',
+        maxWidth: 360,
+        bgcolor: 'background.paper',
+      }}
+    >
+      
     <Stack alignItems='center' spacing={3}>
       <Stack direction='row' spacing={4}>
+      <ListItem>
         {organization.name && (
           <Stack direction='row' alignItems='center' spacing={2}>
             <LocationOnIcon />
             <Typography variant='body2'>{organization.name}</Typography>
           </Stack>
         )}
-
+      </ListItem>
+      <ListItem>
         {organization.email && (
+          
           <Stack direction='row' alignItems='center' spacing={2}>
             <EmailIcon />
             <Typography variant='body2'>{organization.email}</Typography>
           </Stack>
+          
         )}
-
+        </ListItem>
+      <ListItem>
         {organization.phone && (
           <Stack direction='row' alignItems='center' spacing={2}>
             <PhoneIcon />
             <Typography variant='body2'>{organization.phone}</Typography>
           </Stack>
         )}
-
+        </ListItem>
+      <ListItem>
         {organization.website && (
           <Stack direction='row' alignItems='center' spacing={2}>
             <WebsiteIcon />
             <Typography variant='body2'>{organization.website}</Typography>
           </Stack>
         )}
-
+        </ListItem>
+      <ListItem>
         {organization.location && (
           <Stack direction='row' alignItems='center' spacing={2}>
             <Typography variant='body2'>{organization.location}</Typography>
           </Stack>
         )}
-
+        </ListItem>
+      <ListItem>
         {organization.industry && (
           <Stack direction='row' alignItems='center' spacing={2}>
             <Typography variant='body2'>{organization.industry}</Typography>
           </Stack>
         )}
+        </ListItem>
       </Stack>
     </Stack>
+    </List>
   );
 }

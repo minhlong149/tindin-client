b583ca5c4ef2a828996a66f90050d77cc70f3ed1
minhlong149 <minhlong9514@gmail.com>
2023-06-13 09:40:14 +0700

    Improve search UI

--- src/components/Candidate/Search.jsx
+++ src/components/Candidate/Search.jsx
@@ -12,7 +12,11 @@ import {
 import JobService from '../../services/job.js';
 import { useContext } from 'react';
 import { UserContext } from '../../App.jsx';
-
+import Tab from '@mui/material/Tab';
+import Tabs from '@mui/material/Tabs';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 function Search() {
   const user = useContext(UserContext);
   const [searchTerm, setSearchTerm] = useState('');
@@ -54,6 +58,8 @@ function Search() {
   const handleChange = (event) => {
     setCity(event.target.value);
   };
+  const [value, setValue] = useState(0);
+
 
   return (
     <div>
@@ -151,37 +157,56 @@ function Search() {
         </Box>
       </Box>
 
-      <div>
-        <h2>Filtered Organizations:</h2>
-        {isLoading ? (
-          <p>Loading organizations...</p>
-        ) : filteredOrganizations.length > 0 ? (
-          <ul>
-            {filteredOrganizations.map((org) => (
-              <li key={org.id}>{org.name}</li>
-            ))}
-          </ul>
-        ) : (
-          <p>No organizations found.</p>
-        )}
-      </div>
+      <Card sx={{ width: '100%', mx: 5 }}>
+        <Tabs value={value} onChange={(event, newValue) => setValue(newValue)}>
+          <Tab label='Organizations' />
+          <Tab label='Job' />
+        </Tabs>
 
-      <div>
-        <h2>Filtered Jobs:</h2>
-        {isLoading ? (
-          <p>Loading jobs...</p>
-        ) : filteredJobs.length > 0 ? (
-          <ul>
-            {filteredJobs.map((job) =>{
-              console.log(job)
-              return (
-              <li key={job.id}>{job.title}</li>
-            )})}
-          </ul>
-        ) : (
-          <p>No jobs found.</p>
-        )}
-      </div>
+        <TabPanel value={value} index={0}>
+          <div>
+            <h2>Filtered Organizations:</h2>
+            {isLoading ? (
+              <p>Loading organizations...</p>
+            ) : filteredOrganizations.length > 0 ? (
+              <ul>
+                {filteredOrganizations.map((org) => (
+                  <li key={org.id}>{org.name}</li>
+                ))}
+              </ul>
+            ) : (
+              <p>No organizations found.</p>
+            )}
+          </div>
+        </TabPanel>
+
+        <TabPanel value={value} index={1}>
+          <div>
+            <h2>Filtered Jobs:</h2>
+            {isLoading ? (
+              <p>Loading jobs...</p>
+            ) : filteredJobs.length > 0 ? (
+              <ul>
+                {filteredJobs.map((job) => {
+                  console.log(job);
+                  return <li key={job.id}>{job.title}</li>;
+                })}
+              </ul>
+            ) : (
+              <p>No jobs found.</p>
+            )}
+          </div>
+        </TabPanel>
+      </Card>
+    </div>
+  );
+}
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+  return (
+    <div hidden={value !== index} {...other}>
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
     </div>
   );
 }

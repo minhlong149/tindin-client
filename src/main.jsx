cf8837368aeb93019848255cf8009affc553fa31
minhlong149 <minhlong9514@gmail.com>
2023-06-26 15:18:55 +0700

    Update axios defaults baseURL

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://ie303-tindin-job-board.onrender.com';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
